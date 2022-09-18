b86dccb7f9d74ef30ffb324ce176b6607033e74b
Juan Diego <juandiegomariscal@gmail.com>
2022-09-18 15:23:46 +0200

    feat: 👍cargamos frases desde js

--- public/static/js/frases_utiles.js
+++ public/static/js/frases_utiles.js
@@ -1,8 +1,14 @@
 async function mostrar_diccionario(){
-    const response = await fetch("frases.csv");
+    const response = await fetch("frases_comunes.csv");
     const data = await response.text();
     const datos = data.split('\n');
-    console.log(datos)
+    const frases = datos.map(row => row.split(','));
+    const frase = frases.map(row => row[0]);
+    const significado = frases.map(row => row.slice(1));
+    console.log(frase[2])
+    for (i = 0; i < frase.length-1; i++){
+        document.querySelector('#tabla-frases').innerHTML += `<tr><td>${frase[i]}</td><td>${significado[i]}</td></tr>`;
+    }   
 }
 
 window.onload = function() {
