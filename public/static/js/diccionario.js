3d9a51a15edcdf30c99560e5ca06608685aa18ca
Juan Diego <juandiegomariscal@gmail.com>
2022-11-07 17:14:10 +0100

    feat: 👍script para crear entradas de diccionario

--- public/static/js/diccionario.js
+++ public/static/js/diccionario.js
@@ -5,11 +5,22 @@ async function mostrar_diccionario(){
     const palabras = diccionario.map(row => row.split(','));
     const tokipona_word = palabras.map(row => row[0]);
     const significado = palabras.map(row => row.slice(1));
-    for (i = 0; i < tokipona_word.length-1; i++){
-        document.querySelector('#tabla-diccionario').innerHTML += `<tr><td>${tokipona_word[i]}</td><td>${significado[i]}</td></tr>`;
+    for (i = 1; i < tokipona_word.length-1; i++){
+        document.querySelector('#seccion_diccionario').innerHTML += `<div class="entrada_diccionario">
+        <p class="tokipona">${tokipona_word[i]}</p>
+        <p class="description">${tokipona_word[i]}</p>
+        <p class="description">${significado[i]}</p>
+        
+        </div><hr/>`;
     }
 }
+{/* <div class="card">
+<p class="description">toki</p>
+<p class="tokipona">toki</p>
+<p class="description">palabra, idioma, hablar</p>
+</div> */}
 
 window.onload = function() {
     mostrar_diccionario();
 }
+
